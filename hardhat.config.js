a8e18cc7bd3f876100759019a20b00e5fe586473
candostyavuz <cdost@alumni.sabanciuniv.edu>
2021-06-19 13:34:23 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,7 @@ const ALCHEMY_API_KEY = "hrT2yVUia98ibglhZJ7Sy0nCj_rosuOY";
 const ROPSTEN_PRIVATE_KEY = "7f8452750968fa78870158c5d1e7758b07763545de8a2daa89d0f4278fbb4847";
 
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.3",
   networks: {
     ropsten: {
       url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
